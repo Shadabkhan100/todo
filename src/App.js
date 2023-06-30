7f05a57205047fa0e733ed455d9216216d452bb6
Sahibzada Shadab Khan <93811238+Shadabkhan100@users.noreply.github.com>
2023-06-30 17:59:38 +0500

    todo app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Test from './components/Test'
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Test></Test>
+    </>
+  );
+}
+
+export default App;
