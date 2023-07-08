1fe6dbc3b598548c81a008f60f6b0dde8cd92724
Sahibzada Shadab Khan <93811238+Shadabkhan100@users.noreply.github.com>
2023-07-08 19:21:09 +0500

    Add files via upload
    updated files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Test from './components/Test'
+
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Test></Test>
+   
     </>
   );
 }
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,20 +1,41 @@
-import { Button, Checkbox, Form, message } from "antd";
+
+
+import React, { useState } from 'react';
+import { Button, Checkbox, Form, message,Typography } from 'antd';
 import { DeleteFilled, EditFilled } from '@ant-design/icons';
-import TextArea from "antd/es/input/TextArea";
-import React, { useState, useEffect } from "react";
+import TextArea from 'antd/es/input/TextArea';
+import Mymodal from './Mymodal';
+
+const { Title } = Typography;
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
-  const [status, setStatus] = useState("");
+  const [newTodo, setNewTodo] = useState('');
+  const [status, setStatus] = useState('');
   const [editIndex, setEditIndex] = useState(-1);
-const [editTodo, setEditTodo] = useState("");
-const [filter, setFilter] = useState("all");
-const [editStatus, setEditStatus] = useState("");
+  const [editTodo, setEditTodo] = useState('');
+  const [filter, setFilter] = useState('all');
+  const [editStatus, setEditStatus] = useState('');
+  const [visible, setVisible] = useState(false);
 
-const FilterValues=(e) => {
-  setFilter(e.target.value)
-}
+  const handleFilterStatus = (value) => {
+    setStatus(value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { title: newTodo, status: status }]);
+      setNewTodo('');
+      setStatus('');
+      message.success('Your Todo has been added');
+      setVisible(false);
+    }
+  };
+
+  const FilterValues = (e) => {
+    setFilter(e.target.value);
+  };
+  
   const handleEditTodo = (index) => {
     setEditIndex(index);
     setEditTodo(todos[index].title);
@@ -22,157 +43,138 @@ const FilterValues=(e) => {
   };
   
   const FilterStatus = (e) => {
+  
     setStatus(e.target.value);
-  }
-
-  const handleAddTodo = () => {
-    if (newTodo.trim() !== "") {
-      setTodos([...todos, { title: newTodo, status: status }]);
-      setNewTodo("");
-      setStatus("");
-      message.success('Your Todo has been added');
-    }
   };
   
 
+  
   const handleDeleteTodo = (index) => {
     const deletedTodo = todos[index].title;
     const updatedTodos = todos.filter((_, i) => i !== index);
     setTodos(updatedTodos);
     message.success('Your Todo has been deleted');
   };
-
+  
   const handleStatusChange = (index) => {
     const updatedTodos = [...todos];
     updatedTodos[index].status = editStatus;
     setTodos(updatedTodos);
   };
   
-  const SetDate= () => {
-const date=new Date()
-const format={year:'numeric',month:'long',day:'numeric'}
-return date.toLocaleDateString(undefined,format);
+  const SetDate = () => {
+    const date = new Date();
+    const format = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, format);
   };
+  
+
   return (
     <div>
-      <h1 className="text-center my-4">Todo List</h1>
+      <Title className="text-center my-4" level={1} style={{color:'Lightgray'}}>Todo List</Title>
       <div className="container d-flex">
         <div className="container">
-        <button style={{float:'left'}} class="btn btn-success  text-light " type="button" data-bs-toggle="modal" data-bs-target="#signupModal">Add Your Todo</button>
-        <select style={{float:'right'}}  onClick={FilterValues}>
-           <option value="all">All</option> 
-           <option value="incomplete">Incomplete</option>
-           <option value="complete">Complete</option>
-           
-       </select>
-
-                </div>
-      </div>
-{/* Modal for adding the todos */}
-      <div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModal" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="signupModal">Add Your Todo</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form>
-                <label>Enter Your Todo</label>
-                <TextArea type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}></TextArea>
-                <label >Status</label>
-                <select key={"index"} className="my-3 mx-4" onChange={FilterStatus}>
-                  <option value="complete">Complete</option>
-                  <option value="incomplete">Incomplete</option>
-                </select>
-                <div class="modal-footer">
-                  <Button className="mx-4" style={{ float: 'left' }} type="primary" onClick={handleAddTodo}>Add Todo</Button>
-                </div>
-              </form>
-            </div>
-          </div>
+          <Mymodal
+            handleAddTodo={handleAddTodo}
+            newTodo={newTodo}
+            setNewTodo={setNewTodo}
+            setVisible={setVisible}
+            Visible={visible}
+            handleFilterStatus={handleFilterStatus}
+            selectedStatus={status}
+          />
+          <select style={{ float: 'right' }} onClick={FilterValues}>
+            <option value="all">All</option>
+            <option value="incomplete">Incomplete</option>
+            <option value="complete">Complete</option>
+          </select>
         </div>
       </div>
 
+     
 
 
       <div className="container my-3" style={{ border: '.2rem solid gray', backgroundColor: '#cccccc' }}>
-        <h2 className="text-center">Your ToDo</h2>
-        {/* Display when todos lenght is less then 1 */}
-         {todos.length < 1 ? (
-          <p className="text-center " >Add your todo</p>
-        ) : null}
-        <div className="container" >
-          <ul style={{ listStyle: 'none'}}>
-            {/* Filter todos on the bases of the options */}
-          {todos.filter((todo) => {
-  if (filter === "complete") {
-    return todo.status === "complete";
-  } else if (filter === "incomplete") {
-    return todo.status === "incomplete";
-  } else {
-    return true; // Filter is set to "All", show all todos
-  }
-}).map((todo, index) => (
-  <li key={index} className="my-4" style={{backgroundColor:'white',padding:'2rem'}}>
-    {/* Updateing todo if condition true */}
-   {editIndex === index ? (
-    <Form>
-      <Form.Item>
-        <TextArea type="text" value={editTodo} onChange={(e) => setEditTodo(e.target.value)}/>
-      </Form.Item>
-      <Form.Item>
-      <label>Status</label>
-      <select value={editStatus} className="my-3 mx-4" onChange={(e) => setEditStatus(e.target.value)}>
-       <option value="complete">Complete</option>
-      <option value="incomplete">Incomplete</option> 
-    
-    </select>
-      </Form.Item>
-</Form>
-   ) : (
-   <span className="d-flex">
-    {/* check box to show which todo is complete or not */}
-    <Checkbox checked={todo.status === 'incomplete' ? false : true}></Checkbox>
-  <p style={{ fontWeight: 'bold' }} className="mx-3">Title:</p>
-  <p className="mx-3" style={{ textDecoration: todo.status === 'incomplete' ? '' : 'line-through' }}>
-    {todo.title}</p>
-</span>)}
-<p>Updated On: {SetDate()}</p>
-<div className="container p-2">
-{editIndex === index ? (<Button type="primary" onClick={() => {message.success('Your Todo Has Been Updated');
-  handleStatusChange(index);
-  const updatedTodos = [...todos];
-  updatedTodos[index].title = editTodo;
-  setTodos(updatedTodos);
-  setEditIndex(-1);
-}}>
-  Save Update
-</Button>
-
-) : (
-  <>
-    <DeleteFilled
-      type="button"
-      style={{ color: 'blue', float: 'right' }}
-      className="mx-4"
-      onClick={() => handleDeleteTodo(index)}
-    >
-      Delete
-    </DeleteFilled>
-    <EditFilled
-      style={{ color: 'blue', float: 'right', cursor: 'pointer' }}
-      onClick={() => handleEditTodo(index)}
-    />
-  </>
-)}
-</div>
-<hr style={{ color: 'black' }} />
-</li>
-))}
-
-
-         
+        <Title level={2} style={{color:'black'}} className="text-center">Your ToDo</Title>
+        {todos.length < 1 ? <p className="text-center ">Add your todo</p> : null}
+        <div className="container">
+          <ul style={{ listStyle: 'none' }}>
+            {todos
+              .filter((todo) => {
+                if (filter === 'complete') {
+                  return todo.status === 'complete';
+                } else if (filter === 'incomplete') {
+                  return todo.status === 'incomplete';
+                } else {
+                  return true;
+                }
+              })
+              .map((todo, index) => (
+                <li key={index} className="my-4" style={{ backgroundColor: 'white', padding: '2rem' }}>
+                  {editIndex === index ? (
+                    <Form>
+                      <Form.Item>
+                        <TextArea type="text" value={editTodo} onChange={(e) => setEditTodo(e.target.value)} />
+                      </Form.Item>
+                      <Form.Item>
+                        <label>Status</label>
+                        <select
+                          value={editStatus}
+                          className="my-3 mx-4"
+                          onChange={(e) => setEditStatus(e.target.value)}
+                        >
+                          <option value="complete">Complete</option>
+                          <option value="incomplete">Incomplete</option>
+                        </select>
+                      </Form.Item>
+                    </Form>
+                  ) : (
+                    <span className="d-flex">
+                      <Checkbox checked={todo.status === 'incomplete' ? false : true}></Checkbox>
+                      <p style={{ fontWeight: 'bold' }} className="mx-3">
+                        Title:
+                      </p>
+                      <p className="mx-3" style={{ textDecoration: todo.status === 'incomplete' ? '' : 'line-through' }}>
+                        {todo.title}
+                      </p>
+                    </span>
+                  )}
+                  <p>Updated On: {SetDate()}</p>
+                  <div className="container p-2">
+                    {editIndex === index ? (
+                      <Button
+                        type="primary"
+                        onClick={() => {
+                          message.success('Your Todo Has Been Updated');
+                          handleStatusChange(index);
+                          const updatedTodos = [...todos];
+                          updatedTodos[index].title = editTodo;
+                          setTodos(updatedTodos);
+                          setEditIndex(-1);
+                        }}
+                      >
+                        Save Update
+                      </Button>
+                    ) : (
+                      <>
+                        <DeleteFilled
+                          type="button"
+                          style={{ color: 'blue', float: 'right' }}
+                          className="mx-4"
+                          onClick={() => handleDeleteTodo(index)}
+                        >
+                          Delete
+                        </DeleteFilled>
+                        <EditFilled
+                          style={{ color: 'blue', float: 'right', cursor: 'pointer' }}
+                          onClick={() => handleEditTodo(index)}
+                        />
+                      </>
+                    )}
+                  </div>
+                  <hr style={{ color: 'black' }} />
+                </li>
+              ))}
           </ul>
         </div>
       </div>
